30070aac5c1b937cbb78544d272ea9e65ec9cee5
Guido <me@guido.io>
2015-06-01 11:34:36 +0200

    Encapsulate context get in function

--- scrapyroni.py
+++ scrapyroni.py
@@ -2,14 +2,24 @@ import json
 import urllib2
 from credentials import *
 
-base_url = 'http://krabspin.uci.ru.nl'
-action = '/getcontext.json'
+BASE_URL = 'http://krabspin.uci.ru.nl'
+CONTEXT_ACTION = '/getcontext.json'
+
+def get_context(i=0,run_id=0):
+
+    if i < 0 or i >10000:
+        print "Warning: Possibly out of bounds i {0}".format(i)
+
+    if run_id > 10000:
+        print "Run_ID {0} higher than 10,000!".format(run_id)
+        raise
+
+    params = "?i={0}&runid={1}&teamid={2}&teampw={3}".format(i,run_id,TEAM_ID,PASSWORD)
+    request_url = BASE_URL + CONTEXT_ACTION + params
+
+    response = urllib2.urlopen(request_url)
+    return json.load(response)
 
 
-i = 1
-run_id = 1
-
-req_url = base_url + action + "?i={0}&runid={1}&teamid={2}&teampw={3}".format(i,run_id,TEAM_ID,PASSWORD)
-
-swek = urllib2.urlopen(req_url)
-print json.load(swek)
+if __name__ == '__main__':
+    print get_context(0,0)
