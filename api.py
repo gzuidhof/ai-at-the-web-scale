df4a305f885196ab53da9dce81234751aeea8036
Guido Zuidhof <me@guido.io>
2015-06-03 14:42:07 +0200

    Move constants to seperate file

--- api.py
+++ api.py
@@ -1,13 +1,11 @@
 import json
 import urllib2
 from credentials import *
+from constants import *
 
 BASE_URL = 'http://krabspin.uci.ru.nl'
 CONTEXT_ACTION = '/getcontext.json'
 PROPOSAL_ACTION = '/proposePage.json'
-HEADER_TYPES = [5, 15, 35]
-AD_TYPES = ['skyscraper', 'square', 'banner']
-COLOR_TYPES = ['green', 'blue', 'red', 'black', 'white']
 
 def get_context(i=0,run_id=0):
 
@@ -23,20 +21,20 @@ def get_context(i=0,run_id=0):
 
     response = urllib2.urlopen(request_url)
     return json.load(response)
-    
+
 def propose_page(i, run_id, header, adtype, color, productid, price):
     if i < 0 or i >10000:
         print "Warning: Possibly out of bounds i {0}".format(i)
-        
+
     header = int(header)
 
     assert run_id < 10001
     assert header in HEADER_TYPES
     assert adtype in AD_TYPES
     assert color in COLOR_TYPES
-    assert productid >= 10 and productid <= 25
-    assert price >= 0.0 and price <= 50.0
-    
+    assert productid >= MIN_PRODUCT and productid <= MAX_PRODUCT
+    assert price >= MIN_PRICE and price <= MAX_PRICE
+
     params = "?i={0}&runid={1}&teamid={2}&teampw={3}".format(i,run_id,TEAM_ID,PASSWORD)
     params += "&header={0}&adtype={1}&color={2}&productid={3}&price={4}".format(header, adtype, color, productid, price)
     request_url = BASE_URL + PROPOSAL_ACTION + params
---
+++ constants.py
@@ -0,0 +1,10 @@
+HEADER_TYPES = [5, 15, 35]
+AD_TYPES = ['skyscraper', 'square', 'banner']
+COLOR_TYPES = ['green', 'blue', 'red', 'black', 'white']
+
+MAX_PRICE = 50.0
+MIN_PRICE = 0.0
+
+PRODUCT = range(10,26)
+MAX_PRODUCT = PRODUCT[-1]
+MIN_PRODUCT = PRODUCT[ 0]
